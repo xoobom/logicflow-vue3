3649c776f27f19e216dba598ef9c6e896dadc2d6
lifuhai <fuhai.li@gientech.com>
2024-11-15 09:13:11 +0800

    logicflow-vue2

--- src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-//修复找不到.vue类型声明
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  const vueComponent: DefineComponent<{}, {}, any>;
-  export default vueComponent;
-}
